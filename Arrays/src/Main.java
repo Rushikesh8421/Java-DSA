4097f00ec338b3f846aba8447d7f43575e456f16
Rushikesh8421 <patilrushikesh8421@gmail.com>
2023-11-04 19:32:17 +0530

    Arrays and arrayList in ava

--- Arrays/src/Main.java
+++ Arrays/src/Main.java
@@ -1,5 +1,23 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
+        int[][] arr1 = new int[3][3];
+        Scanner sc = new Scanner(System.in);
+
+        for(int row=0;row< arr1.length;row++)
+        {
+            for(int col = 0; col < arr1[row].length; col++)
+            {
+                arr1[row][col] = sc.nextInt();
+            }
+            System.out.println();
+        }
+
+        for (int[] ints : arr1) {
+            System.out.println(Arrays.toString(ints));
+        }
     }
 }
